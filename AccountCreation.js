c5c17717f7a92e9709ebe02d30356ed4351ddef8
E-corp Company Group <116536027+E-CorpCompanyGroup@users.noreply.github.com>
2023-03-06 08:51:06 +0300

    Add files via upload

---
+++ AccountCreation.js
@@ -0,0 +1,15 @@
+  CreateAccount=()=>{
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+      fetch(form.action, {
+          method : "POST",
+          body: new FormData(form),
+      }).then(
+          response => response.json()
+      ).then((html) => {
+       UserCreated()
+        console.log('Data Sent To The DataBase');
+      });
+    });
+  }
+ 
